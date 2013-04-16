5a55a2279327a7d6dce5e58c2eac349910d037fc
grindhold_laptop <grindhold@gmx.net>
2013-04-16 14:40:09 +0200

    ade script independent from wget

--- dgs.py
+++ dgs.py
@@ -11,12 +11,12 @@
 * Die drei konstanten HOST, GALERY_ID und COUNT muessen vor dem ausfuehren 
 * gesetzt werden.
 """
-
 HOST = "" # Der Host auf dem die Galerie liegt (bspw: 1024lan.de)
 GALERY_ID = "" # Die Galerie, zu finden in der URL eines einzelnen bildes (bspw: 20130908-C3D952)
 COUNT = 0 # Die Anzahl der in der Galerie vorhandenen Bilder (bspw: 50)
 
 import os
+import urllib2
 
 i = 0
 a = ""
@@ -31,4 +31,10 @@ for i in range(1,COUNT+1,1):
 
 	source = "http://%s/gallery/archive/%s/0%s.jpg"%(HOST, GALERY_ID, a)
 	print source
-	os.system("wget "+source)
+	
+	f = urllib2.urlopen(source)
+	data = f.read()
+	f.close()
+	target = open("0%s.jpg"%a,"wb")
+	target.write(data)
+	target.close()
